ecdf94d0e94a7d43f79db3fa23f7c425f7d18f69
Mojisan <106364612+Mojisan@users.noreply.github.com>
2024-10-27 20:00:14 +0700

    fix: next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,8 @@
-module.exports = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
   reactStrictMode: true,
-}
+};
+
+export default nextConfig;
